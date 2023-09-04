0ca851cb83276b737d3613f9b893ef0cda0723dd
msoues <murielle.soues@gmail.com>
2023-09-04 14:01:23 +0200

    foreach example

--- src/main.js
+++ src/main.js
@@ -1,15 +1,42 @@
 class Main {
-    constructor() {
-        this.#run()
-    }
+	#peoples = ["Robert Smith", "Billy Idol", "Nina Hagen", "Sid Vicious"];
 
-    #run() {
-        const title = document.querySelector('h1')
-        title.innerText = 'CD Application'
-    }
+	constructor() {
+		this.#run();
+	}
+
+	#run() {
+		const title = document.querySelector("h1");
+		title.innerText = "JS";
+
+		// Add an element in the DOM
+		const titleLevel2 = document.createElement("h2");
+		titleLevel2.innerHTML = "Titre niveau 2";
+
+		// Hook the new DOM Element to an existing lement
+		document.body.appendChild(titleLevel2);
+		const ul = document.createElement("ul");
+
+		// this > fait référence à l'instance de la classe, à l'objet (pas la classe)
+		for (const people of this.#peoples) {
+			const li = document.createElement("li");
+			// ! not
+			// == comparaison valeur et type
+			if (people !== "Billy Idol") {
+				li.innerText = people;
+			} else {
+				li.innerHTML = `<strong>${people}</strong>`;
+			}
+
+			ul.appendChild(li);
+		}
+		document.body.appendChild(ul);
+
+        
+	}
 }
 
 // Self callable function to run the Main class
 (function () {
-    const app = new Main()
-})()
+	const app = new Main();
+})();
